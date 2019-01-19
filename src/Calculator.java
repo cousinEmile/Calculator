ca8cb68b2646a640d64c613b8b85491268cc3dcc
Albert Richard <albertbayatlantic@gmail.com>
2019-01-19 16:50:54 -0500

    third commit attempt dealing with merge conflict

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-    public static double add(double num1, double num2, double num3){return num1+num2+num3;}
+    public static double add(double num1, double num2){return num1+num2;}
 
     public static double subtract(double num1, double num2){return num1-num2;}
 
