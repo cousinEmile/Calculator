f007e8907c9f73fab00c9d10d2004ecf84de4792
Albert Richard <albertbayatlantic@gmail.com>
2019-01-19 17:09:57 -0500

    fixed divs

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,12 +6,10 @@ public class Calculator {
 
     public static double multiply(double num1, double num2){return num1*num2;}
     
-<<<<<<< HEAD
+
     public static double divide(double num1, double num2){return num1/num2;}
 
-=======
-    public static double divide(double product, double divisor({ quotient = num1/num2; return quotient;}
->>>>>>> 4d0b5368e9f9b1a34a4b9bc1ee862e39dbe4f7fb
+
                                                          
                                                          
                                                          
